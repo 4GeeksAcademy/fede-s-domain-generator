d19db166a6cb22c6415f02b9dba25b9b5fb03cbc
Federico Serron Umpierrez <77068927+federico-serron@users.noreply.github.com>
2025-01-19 01:06:09 +0000

    Generador de dominiones terminado

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronoun = ["the", "our", "your"];
+  let adj = ["great", "big", "best", "longest", "awsome", "boring", "happy"];
+  let noun = ["jogger", "racoon", "coder", "website", "app", "hamburger"];
+  let domains = [".com", ".es", ".uy", ".net"];
+
+  function generateDomain() {
+    let newPronoun = pronoun[Math.floor(Math.random() * pronoun.length)];
+    let newAdj = adj[Math.floor(Math.random() * adj.length)];
+    let newNoun = noun[Math.floor(Math.random() * noun.length)];
+    let newDomain = domains[Math.floor(Math.random() * domains.length)];
+
+    return `${newPronoun}${newAdj}${newNoun}${newDomain}`;
+  }
+
+  document.getElementById("btn").onclick = function() {
+    document.getElementById("domain").innerHTML = generateDomain();
+  };
+
+  console.log("Hello Domain Generator from the console!");
 };
