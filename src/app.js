34daf8d9cf1ad4fc9576eb9a985ea61def7cf8d0
Federico Serron Umpierrez <77068927+federico-serron@users.noreply.github.com>
2025-01-23 00:27:25 +0000

    Usando for anidados para generar nombres

--- src/app.js
+++ src/app.js
@@ -8,22 +8,61 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
 
-  let pronoun = ["the", "our", "your"];
-  let adj = ["great", "big", "best", "longest", "awsome", "boring", "happy"];
-  let noun = ["jogger", "racoon", "coder", "website", "app", "hamburger"];
+  let pronouns = ["the", "our", "your", "my", "only"];
+  let adjs = ["great", "big", "best", "longest", "awsome", "boring", "happy"];
+  let nouns = ["jogger", "racoon", "coder", "website", "app", "hamburger"];
   let domains = [".com", ".es", ".uy", ".net"];
 
-  function generateDomain() {
-    let newPronoun = pronoun[Math.floor(Math.random() * pronoun.length)];
-    let newAdj = adj[Math.floor(Math.random() * adj.length)];
-    let newNoun = noun[Math.floor(Math.random() * noun.length)];
-    let newDomain = domains[Math.floor(Math.random() * domains.length)];
+  // function generateDomain() {
+  //   let newPronoun = pronoun[Math.floor(Math.random() * pronoun.length)];
+  //   let newAdj = adj[Math.floor(Math.random() * adj.length)];
+  //   let newNoun = noun[Math.floor(Math.random() * noun.length)];
+  //   let newDomain = domains[Math.floor(Math.random() * domains.length)];
 
-    return `${newPronoun}${newAdj}${newNoun}${newDomain}`;
+  //   return `${newPronoun}${newAdj}${newNoun}${newDomain}`;
+  // }
+
+  function generateDomain(pronouns, adjs, nouns, domains) {
+    let generatedDomains = [];
+    for (
+      let i = Math.floor(Math.random() * pronouns.length);
+      i < pronouns.length;
+      i++
+    ) {
+      for (
+        let j = Math.floor(Math.random() * adjs.length);
+        j < adjs.length;
+        j++
+      ) {
+        for (
+          let x = Math.floor(Math.random() * nouns.length);
+          x < nouns.length;
+          x++
+        ) {
+          for (
+            let y = Math.floor(Math.random() * domains.length);
+            y < domains.length;
+            y++
+          ) {
+            generatedDomains.push(
+              `${pronouns[i]}${adjs[j]}${nouns[x]}${domains[y]}`
+            );
+          }
+        }
+      }
+    }
+    return generatedDomains[
+      Math.floor(Math.random() * generatedDomains.length)
+    ];
   }
 
   document.getElementById("btn").onclick = function() {
-    document.getElementById("domain").innerHTML = generateDomain();
+    document.getElementById("domain").innerHTML = generateDomain(
+      pronouns,
+      adjs,
+      nouns,
+      domains
+    );
   };
 
   console.log("Hello Domain Generator from the console!");
