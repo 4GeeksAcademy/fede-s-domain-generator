f794f652446d55840f8c90d5d6274b5cb0e6b5fc
Federico Serron <fede20041@gmail.com>
2025-01-25 11:00:11 +1100

    Se modifico las indentaciones

--- src/app.js
+++ src/app.js
@@ -10,19 +10,13 @@ window.onload = function() {
 
   let pronouns = ["the", "our", "your", "my", "only", "for", "until"];
   let adjs = ["great", "big", "best", "longest", "awsome", "boring", "happy"];
-  let nouns = [
-    "jogger",
-    "racoon",
-    "coder",
-    "website",
-    "app",
-    "hamburger",
-    "shark"
-  ];
+  let nouns = ["jogger", "racoon", "coder", "website", "app", "hamburger", "shark"];
   let domains = [".com", ".es", ".uy", ".net", ".ar", ".org", "del"];
 
   function generateDomain(pronouns, adjs, nouns, domains) {
+    
     let generatedDomains = [];
+
     for (const pronoun of pronouns) {
       for (const adj of adjs) {
         for (const noun of nouns) {
@@ -36,13 +30,7 @@ window.onload = function() {
   }
 
   document.getElementById("btn").onclick = function() {
-    document.getElementById("domain").innerHTML = generateDomain(
-      pronouns,
-      adjs,
-      nouns,
-      domains
-    );
+    document.getElementById("domain").innerHTML = generateDomain(pronouns, adjs, nouns, domains);
   };
-
   console.log("Hello Domain Generator from the console!");
 };
